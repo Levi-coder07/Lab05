cf59edeb5d9b9f15233076a057d9143659e8fbfc
Levi Castillon <82822546+Levi-coder07@users.noreply.github.com>
2022-11-15 12:28:06 -0500

    Add files via upload

---
+++ Lab5Xunit/hola.py
@@ -0,0 +1,35 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class ChromeSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+    def test_eight_components(self):
+        driver = self.driver
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+        title = driver.title
+        assert title == "Web form"
+
+    def second_test(self):
+        driver = self.driver
+        driver.implicitly_wait(5)
+
+        text_box = driver.find_element(by=By.NAME, value="my-text")
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+        
+        text_box.send_keys("Selenium")
+        submit_button.click()
+
+        message = driver.find_element(by=By.ID, value="message")
+        value = message.text
+        assert value == "Received!"
+        
+        driver.quit()   
+if __name__ == "__main__":
+    unittest.main()
